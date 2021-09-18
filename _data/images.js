c7778bc3115585191eba898cd2fff6bf02acf248
Arpit Batra <arpitbatra123@gmail.com>
2021-09-18 20:04:20 +0530

    add support for webp images

--- _data/images.js
+++ _data/images.js
@@ -1,6 +1,7 @@
 const fs = require('fs'),
   path = require('path'),
-  IMAGE_EXTENSIONS = ['jpeg', 'jpg', 'png'];
+  // Convert all images to png beforehand
+  IMAGE_EXTENSIONS = ['png'];
 
 module.exports = function getImages() {
   return new Promise((resolve, reject) => {
@@ -12,6 +13,10 @@ module.exports = function getImages() {
       files = files.filter((file) => {
         const extension = file.split('.')[1];
         return IMAGE_EXTENSIONS.includes(extension);
+      });
+
+      files = files.map((file) => {
+        return file.split('.')[0];
       });
 
       return resolve(files);
---
+++ makeWebP.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const CWebp = require('cwebp').CWebp;
+
+fs.readdir(path.resolve('inspo'), (_err, files) => {
+  files.forEach((file) => {
+    const fileName = file.split('.')[0];
+
+    const webPFile = `inspo/webp/${fileName}.webp`;
+
+    if (!fs.existsSync(webPFile)) {
+      const encoder = new CWebp(path.resolve('inspo', file));
+      encoder.quality(90);
+      encoder.write(webPFile, function (err) {
+        console.log(err || 'encoded successfully');
+      });
+    }
+  });
+});
