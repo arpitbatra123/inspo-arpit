55be1be467c3eebfb668516172658f58a688840a
Arpit Batra <arpitbatra123@gmail.com>
2024-09-29 13:22:04 +0530

    Refactor eleventy config and update background colors

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy('inspo');
   eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addPassthroughCopy('favicon.ico');
 
- eleventyConfig.addPlugin(pluginRss);
+  // Disable rss plugin until it's fixed
+  //  eleventyConfig.addPlugin(pluginRss);
 };
