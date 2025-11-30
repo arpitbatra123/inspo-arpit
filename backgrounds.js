420b0a986438a79436f635c4254893629c49e8bd
Arpit Batra <arpitbatra123@gmail.com>
2025-11-30 22:27:07 +0530

    Consolidate to single masonry layout: remove legacy pages, dynamic backgrounds, lightbox enhancements, build pipeline updates

--- backgrounds.js
+++ backgrounds.js
@@ -1,11 +1,11 @@
 // https://github.com/arpitbatra123/inspo-arpit/issues/2
 
-const fs = require('fs'),
-  path = require('path'),
-  // Convert all images to png beforehand
-  IMAGE_EXTENSIONS = ['png'],
-  ColorThief = require('colorthief'),
-  colorsArr = {};
+const fs = require('fs');
+const path = require('path');
+// Convert all images to png beforehand
+const IMAGE_EXTENSIONS = ['png'];
+const ColorThief = require('colorthief');
+const colorsArr = {};
 
 const rgbToHex = (r, g, b) =>
   '#' +
@@ -17,19 +17,40 @@ const rgbToHex = (r, g, b) =>
     .join('');
 
 async function getDominantColor() {
-  let files = await fs.promises.readdir(path.resolve('inspo'));
-  files = files.filter((file) => {
-    const extension = file.split('.')[1];
-    return IMAGE_EXTENSIONS.includes(extension);
-  });
+  try {
+    let files = await fs.promises.readdir(path.resolve('inspo'));
+    files = files.filter((file) => {
+      const extension = file.split('.')[1];
+      return IMAGE_EXTENSIONS.includes(extension);
+    });
 
-  for (const file of files) {
-    const img = path.resolve(`inspo/${file}`);
-    const colors = await ColorThief.getColor(img);
-    colorsArr[file] = colors ? rgbToHex(...colors) : 'thistle';
+    for (const file of files) {
+      const img = path.resolve(`inspo/${file}`);
+      try {
+        const colors = await ColorThief.getColor(img);
+        colorsArr[file] = colors ? rgbToHex(...colors) : 'thistle';
+      } catch (e) {
+        colorsArr[file] = 'thistle';
+      }
+    }
+
+    // Sort keys numerically if they start with numbers to keep file deterministic
+    const sorted = Object.keys(colorsArr)
+      .sort((a, b) => parseInt(a) - parseInt(b))
+      .reduce((acc, key) => {
+        acc[key] = colorsArr[key];
+        return acc;
+      }, {});
+
+    const outPath = path.resolve('background.json');
+    await fs.promises.writeFile(outPath, JSON.stringify(sorted, null, 4));
+    console.log(`[backgrounds] wrote ${Object.keys(sorted).length} entries to background.json`);
+  } catch (err) {
+    console.error('[backgrounds] failed', err);
+    process.exitCode = 1;
   }
-
-  console.log(JSON.stringify(colorsArr, null, 4));
 }
 
-getDominantColor();
+if (require.main === module) {
+  getDominantColor();
+}
